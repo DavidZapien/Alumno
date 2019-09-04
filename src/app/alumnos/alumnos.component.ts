9c27259c508c4002cc4685e5e97d0afaf737c004
DavidZapien <David_cito_13@hotmail.com>
2019-09-04 18:18:20 -0500

    Modifica el archivo src/app/alumno/alumno.component.ts

--- src/app/alumnos/alumnos.component.ts
+++ src/app/alumnos/alumnos.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { alumno } from '../alumno';
 @Component({
-  selector: 'app-alumnos',
-  templateUrl: './alumnos.component.html',
-  styleUrls: ['./alumnos.component.css']
+selector: 'app-alumnos',
+templateUrl: './alumnos.component.html',
+styleUrls: ['./alumnos.component.css']
 })
 export class AlumnosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+alumno: alumno= {
+id: 1,
+name: 'David'
+};
+constructor() { }
+ngOnInit() {
+}
 }
