f38d4eb95a04c43e3f4ed223cea193040efbeb69
DavidZapien <David_cito_13@hotmail.com>
2019-09-04 18:09:41 -0500

    Genera un componente en angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlumnosComponent } from './alumnos/alumnos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlumnosComponent
   ],
   imports: [
     BrowserModule,
