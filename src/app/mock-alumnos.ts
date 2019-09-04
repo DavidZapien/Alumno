fc748404e38fb0e98e123d96f8795929b446a890
DavidZapien <David_cito_13@hotmail.com>
2019-09-04 18:28:30 -0500

    Crea el archivo src/app/mock-alumnos.ts

---
+++ src/app/mock-alumnos.ts
@@ -0,0 +1,13 @@
+import { alumno } from './alumno';
+export const ALUMNO: alumno[] = [
+{ id: 11, name: 'Marco' },
+{ id: 12, name: 'Fabian' },
+{ id: 13, name: 'Blanca' },
+{ id: 14, name: 'Aranza' },
+{ id: 15, name: 'David' },
+{ id: 16, name: 'Alexis' },
+{ id: 17, name: 'Leonardo' },
+{ id: 18, name: 'Ruben' },
+{ id: 19, name: 'Issac' },
+{ id: 20, name: 'Pepe' }
+];
